bca8ddf4d7c788aac07aacfbb2f5cc6fd6edf8d9
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-08-14 16:47:54 -0400

    refactor: update main App component for new onboarding and layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { LaunchScreen } from "@/components/LaunchScreen"
 import { ThemeProvider } from "@/contexts/ThemeContext"
 import { AuthProvider } from "@/contexts/AuthContext"
+import { ErrorBoundary } from "@/components/ErrorBoundary"
 
 function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <LaunchScreen />
-      </AuthProvider>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <AuthProvider>
+          <LaunchScreen />
+        </AuthProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   )
 }
 
