8e2c22bf8e193c1f97d54ec1e682b942067b2ca4
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-08-14 16:47:31 -0400

    chore: add buffer type definitions

---
+++ src/types/buffer.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Buffer polyfill type declarations
+ */
+
+declare global {
+  interface Window {
+    Buffer?: {
+      from(data: any, encoding?: string): Uint8Array;
+      isBuffer(obj: any): boolean;
+    };
+  }
+  
+  var Buffer: {
+    from(data: any, encoding?: string): Uint8Array;
+    isBuffer(obj: any): boolean;
+  };
+}
+
+export {};
