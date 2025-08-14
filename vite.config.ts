c159d5b5394114d27d98708bb0d22ae4005b51f9
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-08-14 16:50:40 -0400

    chore: update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig
-} from "vite"
+import { defineConfig } from "vite"
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname,
-      "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ['bip39', 'ethers']
+  }
 })
