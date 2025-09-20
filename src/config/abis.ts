75f1a73340be745a3a876a62fe6a9cc01cd4660b
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-09-21 02:31:41 +0530

    Add cross-chain withdrawal ABI and update constants
    Added the processCrossChainWithdrawal function ABI to PRIVACY_POOL_ENTRYPOINT_ABI. Updated contract addresses and increased the default relay fee to 15% in constants.ts. Added new escrow and oracle addresses for cross-chain operations.

--- src/config/abis.ts
+++ src/config/abis.ts
@@ -85,6 +85,137 @@ export const PRIVACY_POOL_ENTRYPOINT_ABI = [
     stateMutability: "payable",
     type: "function",
   },
+  {
+  "inputs": [
+    {
+      "components": [
+        {
+          "internalType": "address",
+          "name": "processooor",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes",
+          "name": "data",
+          "type": "bytes"
+        }
+      ],
+      "internalType": "struct IShinobiCashCrossChainHandler.CrossChainWithdrawal",
+      "name": "withdrawal",
+      "type": "tuple"
+    },
+    {
+      "components": [
+        {
+          "internalType": "uint256[2]",
+          "name": "pA",
+          "type": "uint256[2]"
+        },
+        {
+          "internalType": "uint256[2][2]",
+          "name": "pB",
+          "type": "uint256[2][2]"
+        },
+        {
+          "internalType": "uint256[2]",
+          "name": "pC",
+          "type": "uint256[2]"
+        },
+        {
+          "internalType": "uint256[9]",
+          "name": "pubSignals",
+          "type": "uint256[9]"
+        }
+      ],
+      "internalType": "struct IShinobiCashCrossChainHandler.CrossChainWithdrawProof",
+      "name": "proof",
+      "type": "tuple"
+    },
+    {
+      "internalType": "uint256",
+      "name": "scope",
+      "type": "uint256"
+    },
+    {
+      "components": [
+        {
+          "internalType": "uint32",
+          "name": "fillDeadline",
+          "type": "uint32"
+        },
+        {
+          "internalType": "uint32",
+          "name": "expires",
+          "type": "uint32"
+        },
+        {
+          "internalType": "address",
+          "name": "inputOracle",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256[2][]",
+          "name": "inputs",
+          "type": "uint256[2][]"
+        },
+        {
+          "components": [
+            {
+              "internalType": "bytes32",
+              "name": "oracle",
+              "type": "bytes32"
+            },
+            {
+              "internalType": "bytes32",
+              "name": "settler",
+              "type": "bytes32"
+            },
+            {
+              "internalType": "uint256",
+              "name": "chainId",
+              "type": "uint256"
+            },
+            {
+              "internalType": "bytes32",
+              "name": "token",
+              "type": "bytes32"
+            },
+            {
+              "internalType": "uint256",
+              "name": "amount",
+              "type": "uint256"
+            },
+            {
+              "internalType": "bytes32",
+              "name": "recipient",
+              "type": "bytes32"
+            },
+            {
+              "internalType": "bytes",
+              "name": "call",
+              "type": "bytes"
+            },
+            {
+              "internalType": "bytes",
+              "name": "context",
+              "type": "bytes"
+            }
+          ],
+          "internalType": "struct MandateOutput[]",
+          "name": "outputs",
+          "type": "tuple[]"
+        }
+      ],
+      "internalType": "struct IShinobiCashCrossChainHandler.CrossChainIntentParams",
+      "name": "intentParams",
+      "type": "tuple"
+    }
+  ],
+  "name": "processCrossChainWithdrawal",
+  "outputs": [],
+  "stateMutability": "nonpayable",
+  "type": "function"
+  }
 ] as const;
 
 // ============ UTILITY TYPES ============
